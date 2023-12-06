d8ae569958a8ce7220c2ea4afd97808b9d82cf1f
Soome-03042003 <102472963+Soome-03042003@users.noreply.github.com>
2023-12-06 23:52:54 +0700

    video 16

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import Component from "./component/Component";
 const App = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Tran Van Loc</p>
+        <p>Tran Van Loc cc</p>
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+      </header> */}
+      <Component />
     </div>
   );
 };
---
+++ src/component/Component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Component extends React.Component {
+  //JSX
+  render() {
+    return (
+      <div>
+        my fist Component <br></br>
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default Component;
