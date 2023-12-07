a056f78bc78c0d92343bf3b13018027306aa5535
Soome-03042003 <102472963+Soome-03042003@users.noreply.github.com>
2023-12-07 21:57:14 +0700

    upload file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import Component from "./component/Component";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
 
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Tran Van Loc cc</p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header> */}
-      <Component />
-    </div>
-  );
-};
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Component />
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Tran Van Loc cc</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+
+//     </div>
+//   );
+// };
 
 export default App;
--- src/component/Component.js
+++ src/component/Component.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 class Component extends React.Component {
+  state = {
+    name: "Tran Van Loc",
+    address: "Thua Thien Hue",
+    age: 20,
+  };
+
   //JSX
   render() {
     return (
       <div>
-        my fist Component <br></br>
-        {Math.random()}
+        My Name Ís {this.state.name} and my age is {this.state.age}
+        <h1>My Address is {this.state.address}</h1>
       </div>
     );
   }
