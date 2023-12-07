4107fd90008e8577b09bec2dd23808a05411932b
Soome-03042003 <102472963+Soome-03042003@users.noreply.github.com>
2023-12-07 22:55:52 +0700

    upload file

--- src/component/Component.js
+++ src/component/Component.js
@@ -6,13 +6,20 @@ class Component extends React.Component {
     address: "Thua Thien Hue",
     age: 20,
   };
-
+  handleClick(state) {
+    console.log("click me");
+  }
+  handleHoverMe(event) {
+    console.log(event.pageX);
+  }
   //JSX
   render() {
     return (
       <div>
         My Name Ís {this.state.name} and my age is {this.state.age}
         <h1>My Address is {this.state.address}</h1>
+        <button onClick={this.handleClick}>Click me???</button>
+        <button onMouseOver={this.handleHoverMe}>Click me???</button>
       </div>
     );
   }
