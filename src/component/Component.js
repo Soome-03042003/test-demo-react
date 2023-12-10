a3777a317a864cb90836f0917e1eb024826455d9
Soome-03042003 <102472963+Soome-03042003@users.noreply.github.com>
2023-12-10 23:40:59 +0700

    file 31

--- src/component/Component.js
+++ src/component/Component.js
@@ -5,9 +5,9 @@ import DisplayInfor from "./DisplayInfor";
 class Component extends React.Component {
   state = {
     listUser: [
-      { id: 1, name: "Tran Van Loc", age: 12 },
-      { id: 2, name: "Tran Van A", age: 12 },
-      { id: 3, name: "Tran Van Z", age: 12 },
+      { id: 1, name: "Tran Van Loc", age: 16 },
+      { id: 2, name: "Tran Van A", age: 25 },
+      { id: 3, name: "Tran Van Z", age: 40 },
     ],
   };
 
--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -1,20 +1,75 @@
 import React from "react";
-
+import "./DisplayInfor.scss";
 class DisplayInfor extends React.Component {
+  state = {
+    thisShowList: true,
+  };
+  handleShowHidden = () => {
+    this.setState({
+      thisShowList: false,
+    });
+  };
   render() {
     const { listUser } = this.props;
+    console.table(listUser);
     return (
-      <div>
-        {listUser.map((user, index) => {
-          console.log(index);
-          return (
-            <div key={user.id}>
-              <h1>Ten Toi La {user.name}</h1>
-              <br />
-              <h2>Tuoi : {user.age}</h2>
-            </div>
-          );
-        })}
+      <div className="DisplayInfor-container">
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHidden();
+            }}
+          >
+            {this.state.thisShowList === true
+              ? "hiden list User"
+              : "Show List User"}
+          </span>
+        </div>
+        {this.state.thisShowList && (
+          <div>
+            {listUser.map((user, index) => {
+              console.log(index);
+
+              return (
+                <div className={+user.age > 18 ? "red" : "blue"} key={user.id}>
+                  <h1 style={{ paddingTop: "50px", color: "pink" }}>
+                    Ten Toi La {user.name}
+                  </h1>
+                  <br />
+                  <h2>Tuoi : {user.age}</h2>
+                </div>
+              );
+
+              // if (+user.age > 18 && +user.age < 25) {
+              //   // +user :chuyen tu string sang number
+              //   return (
+              //     <div className="red" key={user.id}>
+              //       <h1>Ten Toi La {user.name}</h1>
+              //       <br />
+              //       <h2>Tuoi : {user.age}</h2>
+              //     </div>
+              //   );
+              // } else if (+user.age >= 25 && +user.age < 39) {
+              //   return (
+              //     <div className="green" key={user.id}>
+              //       <h1>Ten Toi La {user.name}</h1>
+              //       <br />
+              //       <h2>Tuoi : {user.age}</h2>
+              //     </div>
+              //   );
+              // } else {
+              //   return (
+              //     <div className="blue" key={user.id}>
+              //       <h1>Ten Toi La {user.name}</h1>
+              //       <br />
+              //       <h2>Tuoi : {user.age}</h2>
+              //     </div>
+              //   );
+              // }
+            })}
+            :
+          </div>
+        )}
       </div>
     );
     //destructuring array
